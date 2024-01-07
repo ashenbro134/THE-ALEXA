3d0cefc132be3352e4b67abbded90657d0126cd1
ashenbro134 <129660680+ashenbro134@users.noreply.github.com>
2024-01-07 19:45:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ import { watchFile, unwatchFile } from 'fs'
 import cfonts from 'cfonts';
 import { createInterface } from 'readline'
 import yargs from 'yargs'
-import express from 'express'
-import chalk from 'chalk'
-import path from 'path'
-import os from 'os'
-import { promises as fsPromises } from 'fs'
 
 // https://stackoverflow.com/a/50052194
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -21,30 +16,25 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-const app = express()
-const port = process.env.PORT || 8080;
-
-say('Alexa', {
+say('Dylux FG98', {
   font: 'pallet',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`Alexa Bot by ashen: @ashenbro134`, {
+say(`dylux-bot By FG98 Ig: @fg98_ff`, {
   font: 'console',
   align: 'center',
   gradient: ['cyan', 'magenta']
 })
 
-app.listen(port, () => {
-  console.log(chalk.green(`🌐 Puerto ${port} esta abierto`));
-});
-
 var isRunning = false
-
-async function start(file) {
+/**
+ * Start a js file
+ * @param {String} file `path/to/file`
+ */
+function start(file) {
   if (isRunning) return
   isRunning = true
-  const currentFilePath = new URL(import.meta.url).pathname
   let args = [join(__dirname, file), ...process.argv.slice(2)]
   say([process.argv[0], ...args].join(' '), {
     font: 'console',
@@ -70,49 +60,15 @@ async function start(file) {
     }
   })
   //---
-  p.on('exit', (_, code) => {
+ p.on('exit', (_, code) => {
     isRunning = false
     console.error('❎ Ocurrió un error inesperado:', code)
-    start('main.js'); //
-
     if (code === 0) return
     watchFile(args[0], () => {
       unwatchFile(args[0])
       start(file)
     })
   })
-
-  //---
-  console.log(chalk.yellow(`🖥️ ${os.type()}, ${os.release()} - ${os.arch()}`));
-  const ramInGB = os.totalmem() / (1024 * 1024 * 1024);
-  console.log(chalk.yellow(`💾 Total RAM: ${ramInGB.toFixed(2)} GB`));
-  const freeRamInGB = os.freemem() / (1024 * 1024 * 1024);
-  console.log(chalk.yellow(`💽 Free RAM: ${freeRamInGB.toFixed(2)} GB`));
-  console.log(chalk.yellow(`📃 Script by FG98`));
-
-  const packageJsonPath = path.join(path.dirname(currentFilePath), './package.json');
-    try {
-    const packageJsonData = await fsPromises.readFile(packageJsonPath, 'utf-8');
-    const packageJsonObj = JSON.parse(packageJsonData);
-    console.log(chalk.blue.bold(`\n📦 Información del Paquete`));
-    console.log(chalk.cyan(`Nombre: ${packageJsonObj.name}`));
-    console.log(chalk.cyan(`Versión: ${packageJsonObj.version}`));
-    console.log(chalk.cyan(`Descripción: ${packageJsonObj.description}`));
-    console.log(chalk.cyan(`Autor: ${packageJsonObj.author.name}`));
-  } catch (err) {
-    console.error(chalk.red(`❌ No se pudo leer el archivo package.json: ${err}`));
-  }
-
-
-  console.log(chalk.blue.bold(`\n⏰ Hora Actual`));
-  const currentTime = new Date().toLocaleString('es-ES', { timeZone: 'America/Argentina/Buenos_Aires' })
-  //const currentTime = new Date().toLocaleString();
-  console.log(chalk.cyan(`${currentTime}`));
-
-  setInterval(() => {}, 1000);
-
-  
-
   //----
   let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
   if (!opts['test'])
