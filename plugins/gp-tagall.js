af8ea0b3b5661f63b1f316df1a3d4ab18c083f60
ashenbro134 <129660680+ashenbro134@users.noreply.github.com>
2024-01-08 15:15:22 +0530

    Add files via upload

---
+++ plugins/gp-tagall.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+    m.reply(`💝 Group : *${groupMetadata.subject}*\n💝 Members : *${participants.length}*${text ? `\n▢ Message : ${text}\n` : ''}\n┌───⊷ *MENTIONS*\n` + users.map(v => '💝 @' + v.replace(/@.+/, '')).join`\n` + '\n└──💜𝗔𝗟𝗘𝗫𝗔 𝗕𝗢𝗧💜──', null, {
+        mentions: users
+    })
+}
+
+handler.help = ['tagall']
+handler.tags = ['group']
+handler.command = ['tagall']
+handler.admin = true
+handler.group = true
+
+export default handler
